1430a46005edfa41771979aaf32551a1a2bdfac0
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-05-26 19:57:17 +0600

    tailwind and shadcn setup

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,16 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { Button } from "@/components/ui/button";
+import { createLazyFileRoute } from "@tanstack/react-router";
 
-export const Route = createLazyFileRoute('/')({
+export const Route = createLazyFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Homeeeeeee!</h3>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click me</Button>
     </div>
-  )
+  );
 }
