44cf240aca79eb1beac206f1dc676f973a407e10
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-05-31 00:19:50 +0600

    feat: add a minimap for the model

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,78 +15,102 @@ function Index() {
 
     let components = new OBC.Components();
 
-    // set up the world ( A world represents a 3D environment in your application. It consists of a scene, a camera and (optionally) a renderer )
-    let worlds = components.get(OBC.Worlds);
-    let world = worlds.create<
-      OBC.SimpleScene,
-      OBC.OrthoPerspectiveCamera,
-      OBC.SimpleRenderer
-    >();
+    const initBim = async () => {
+      // set up the world ( A world represents a 3D environment in your application. It consists of a scene, a camera and (optionally) a renderer )
+      let worlds = components.get(OBC.Worlds);
+      let world = worlds.create<
+        OBC.SimpleScene,
+        OBC.OrthoPerspectiveCamera,
+        OBC.SimpleRenderer
+      >();
 
-    world.scene = new OBC.SimpleScene(components);
-    world.renderer = new OBC.SimpleRenderer(components, container);
-    world.camera = new OBC.OrthoPerspectiveCamera(components);
+      world.scene = new OBC.SimpleScene(components);
+      world.renderer = new OBC.SimpleRenderer(components, container);
+      world.camera = new OBC.OrthoPerspectiveCamera(components);
 
-    components.init();
+      components.init();
 
-    // it will set some basic lightings and stuffs
-    world.scene.setup();
+      // it will set some basic lightings and stuffs
+      world.scene.setup();
 
-    // make the camera look at the cube
-    world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0);
+      // make the camera look at the cube
+      world.camera.controls.setLookAt(12, 6, 8, 0, 0, -10);
 
-    world.scene.three.background = null;
+      world.scene.three.background = null;
 
-    // cube
-    let cubeGeometry = new THREE.BoxGeometry();
-    let cubeMaterial = new THREE.MeshStandardMaterial({ color: "#6528D7" });
-    let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    cube.position.set(0, 0.5, 0);
+      // cube
+      // let cubeGeometry = new THREE.BoxGeometry();
+      // let cubeMaterial = new THREE.MeshStandardMaterial({ color: "#6528D7" });
+      // let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+      // cube.position.set(0, 0.5, 0);
 
-    world.scene.three.add(cube);
-    world.meshes.add(cube);
+      // world.scene.three.add(cube);
+      // world.meshes.add(cube);
 
-    // grids
-    let grids = components.get(OBC.Grids);
-    let grid = grids.create(world);
-    console.log(grid);
+      // grids
+      let grids = components.get(OBC.Grids);
+      let grid = grids.create(world);
+      console.log(grid);
 
-    world.camera.projection.onChanged.add(() => {
-      const projection = world.camera.projection.current;
-      grid.fade = projection === "Perspective";
-    });
+      world.camera.projection.onChanged.add(() => {
+        const projection = world.camera.projection.current;
+        grid.fade = projection === "Perspective";
+      });
 
-    // need to initialize the Raycaster for this world so that the position of the mouse is tracked from the very first moment we use the clipping planes.
-    const casters = components.get(OBC.Raycasters);
-    casters.get(world);
+      let fragments = new OBC.FragmentsManager(components);
 
-    const clipper = components.get(OBC.Clipper);
+      let file = await fetch(
+        "https://thatopen.github.io/engine_components/resources/small.frag"
+      );
+      let dataBlob = await file.arrayBuffer();
+      let buffer = new Uint8Array(dataBlob);
+      let model = fragments.load(buffer);
+      model.position.set(0, 1, 0);
+      world.scene.three.add(model);
 
-    clipper.enabled = true;
+      // setup minimap
+      const maps = new OBC.MiniMaps(components);
+      const map = maps.create(world);
 
-    container.ondblclick = () => clipper.create(world);
+      const mapContainer = document.getElementById("minimap") as HTMLDivElement;
+      const canvas = map.renderer.domElement;
+      canvas.style.borderRadius = "12px";
+      mapContainer.append(canvas);
+      map.resize();
 
-    window.onkeydown = (event) => {
-      if (event.code === "Delete" || event.code === "Backspace") {
-        clipper.delete(world);
-      }
-    };
+      const mapSize = map.getSize();
 
-    // measure the performance
-    const stats = new Stats();
-    stats.showPanel(2);
-    document.body.append(stats.dom);
-    stats.dom.style.left = "0px";
-    stats.dom.style.zIndex = "unset";
-    world.renderer.onBeforeUpdate.add(() => stats.begin());
-    world.renderer.onAfterUpdate.add(() => stats.end());
+      // need to initialize the Raycaster for this world so that the position of the mouse is tracked from the very first moment we use the clipping planes.
+      const casters = components.get(OBC.Raycasters);
+      casters.get(world);
 
-    // initialize cool ui
-    BUI.Manager.init();
+      const clipper = components.get(OBC.Clipper);
 
-    // inputs panel
-    const panel = BUI.Component.create<BUI.PanelSection>(() => {
-      return BUI.html`
+      clipper.enabled = true;
+
+      container.ondblclick = () => clipper.create(world);
+
+      window.onkeydown = (event) => {
+        if (event.code === "Delete" || event.code === "Backspace") {
+          clipper.delete(world);
+        }
+      };
+
+      // measure the performance
+      const stats = new Stats();
+      stats.showPanel(2);
+      document.body.append(stats.dom);
+      stats.dom.style.left = "0px";
+      stats.dom.style.zIndex = "unset";
+      world.renderer.onBeforeUpdate.add(() => stats.begin());
+      world.renderer.onAfterUpdate.add(() => stats.end());
+
+      // initialize cool ui
+      BUI.Manager.init();
+
+      // inputs panel
+      const panel = BUI.Component.create<BUI.PanelSection>(() => {
+        return BUI.html`
         <bim-panel label="Settings" class="options-menu">
           <bim-panel-section collapsed label="Lights">
           
@@ -165,9 +189,9 @@ function Index() {
           <bim-button 
             label="Rotate cube" 
             @click="${() => {
-              cube.rotation.x = 2 * Math.PI * Math.random();
-              cube.rotation.y = 2 * Math.PI * Math.random();
-              cube.rotation.z = 2 * Math.PI * Math.random();
+              model.rotation.x = 2 * Math.PI * Math.random();
+              model.rotation.y = 2 * Math.PI * Math.random();
+              model.rotation.z = 2 * Math.PI * Math.random();
             }}">  
           </bim-button>
          
@@ -220,9 +244,10 @@ function Index() {
       </bim-checkbox>  
       
       <bim-button 
-        label="Fit cube" 
+        label="Fit model" 
         @click="${() => {
-          world.camera.fit([cube]);
+          // @ts-expect-error: stupid ts err
+          world.camera.fit([model]);
         }}">  
       </bim-button>
 
@@ -246,19 +271,30 @@ function Index() {
 
         world.scene.setup();
 
-        await world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0);
+        await world.camera.controls.setLookAt(12, 6, 8, 0, 0, -10);
 
         components.init();
 
         world.scene.three.background = null;
 
-        cubeGeometry = new THREE.BoxGeometry();
-        cubeMaterial = new THREE.MeshStandardMaterial({ color: "#6528D7" });
-        cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        cube.position.set(0, 0.5, 0);
+        // cubeGeometry = new THREE.BoxGeometry();
+        // cubeMaterial = new THREE.MeshStandardMaterial({ color: "#6528D7" });
+        // cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        // cube.position.set(0, 0.5, 0);
 
-        world.scene.three.add(cube);
-        world.meshes.add(cube);
+        // world.scene.three.add(cube);
+        // world.meshes.add(cube);
+
+        fragments = new OBC.FragmentsManager(components);
+
+        file = await fetch(
+          "https://thatopen.github.io/engine_components/resources/small.frag"
+        );
+        dataBlob = await file.arrayBuffer();
+        buffer = new Uint8Array(dataBlob);
+        model = fragments.load(buffer);
+        model.position.set(0, 1, 0);
+        world.scene.three.add(model);
 
         grids = components.get(OBC.Grids);
         grid = grids.create(world);
@@ -271,14 +307,63 @@ function Index() {
     </bim-button>  
 
     </bim-panel-section>
+    <bim-panel-section collapsed label="Minimap">
+      
+    <bim-checkbox checked="true" label="Enabled" 
+      @change="${({ target }: { target: BUI.Checkbox }) => {
+        map.enabled = target.value;
+      }}">  
+    </bim-checkbox>
+    
+    <bim-checkbox checked label="Lock rotation" 
+      @change="${({ target }: { target: BUI.Checkbox }) => {
+        map.lockRotation = target.value;
+      }}">  
+    </bim-checkbox>
+    
+    <bim-number-input 
+      slider label="Zoom" value="${map.zoom}" min="0.01" max="0.5" step="0.01" 
+      @change="${({ target }: { target: BUI.NumberInput }) => {
+        map.zoom = target.value;
+      }}">
+    </bim-number-input>
+    
+    <bim-number-input 
+      slider label="Front offset" value="${map.frontOffset}" min="0" max="5" step="1" 
+      @change="${({ target }: { target: BUI.NumberInput }) => {
+        map.frontOffset = target.value;
+      }}">
+    </bim-number-input>
+            
+    <div style="display: flex; gap: 12px">
+    
+      <bim-number-input slider value="${mapSize.x}" pref="Size X" min="100" max="500" step="10"              
+        @change="${({ target }: { target: BUI.NumberInput }) => {
+          const size = map.getSize();
+          size.x = target.value;
+          map.resize(size);
+        }}">
+      </bim-number-input>        
+    
+      <bim-number-input slider value="${mapSize.y}" pref="Size Y" min="100" max="500" step="10"            
+        @change="${({ target }: { target: BUI.NumberInput }) => {
+          const size = map.getSize();
+          size.y = target.value;
+          map.resize(size);
+        }}">
+      </bim-number-input>
+    </div>
+
+    
+  </bim-panel-section>
         </bim-panel>
         `;
-    });
+      });
 
-    document.body.append(panel);
+      document.body.append(panel);
 
-    const button = BUI.Component.create<BUI.PanelSection>(() => {
-      return BUI.html`
+      const button = BUI.Component.create<BUI.PanelSection>(() => {
+        return BUI.html`
           <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
             @click="${() => {
               if (panel.classList.contains("options-menu-visible")) {
@@ -289,21 +374,26 @@ function Index() {
             }}">
           </bim-button>
         `;
-    });
+      });
 
-    document.body.append(button);
+      document.body.append(button);
+    };
+
+    initBim();
 
     return () => {
-      clipper.deleteAll();
       components.dispose();
     };
   }, []);
 
   return (
-    <div id="container" className="full-screen">
-      {/* <h3>Welcome Homeeeeeee!</h3>
+    <>
+      <div id="container" className="full-screen">
+        {/* <h3>Welcome Homeeeeeee!</h3>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button>Click me</Button> */}
-    </div>
+      </div>
+      <div className="minimap" id="minimap"></div>
+    </>
   );
 }
