19bcd99c168d3aee8a92575bd4cf2b9c5355a5ce
mehedi-hasan <mdmehedihasan2810@gmail.com>
2024-05-30 23:08:16 +0600

    feat: clipping 3d object

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -29,6 +29,12 @@ function Index() {
 
     components.init();
 
+    // it will set some basic lightings and stuffs
+    world.scene.setup();
+
+    // make the camera look at the cube
+    world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0);
+
     world.scene.three.background = null;
 
     // grids
@@ -40,13 +46,25 @@ function Index() {
     const material = new THREE.MeshLambertMaterial({ color: "#6528D7" });
     const geometry = new THREE.BoxGeometry();
     const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0, 0.5, 0);
     world.scene.three.add(cube);
+    world.meshes.add(cube);
 
-    // it will set some basic lightings and stuffs
-    world.scene.setup();
+    // need to initialize the Raycaster for this world so that the position of the mouse is tracked from the very first moment we use the clipping planes.
+    const casters = components.get(OBC.Raycasters);
+    casters.get(world);
 
-    // make the camera look at the cube
-    world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0);
+    const clipper = components.get(OBC.Clipper);
+
+    clipper.enabled = true;
+
+    container.ondblclick = () => clipper.create(world);
+
+    window.onkeydown = (event) => {
+      if (event.code === "Delete" || event.code === "Backspace") {
+        clipper.delete(world);
+      }
+    };
 
     // measure the performance
     const stats = new Stats();
@@ -63,8 +81,8 @@ function Index() {
     // inputs panel
     const panel = BUI.Component.create<BUI.PanelSection>(() => {
       return BUI.html`
-        <bim-panel label="Worlds Tutorial" class="options-menu">
-          <bim-panel-section collapsed label="Controls">
+        <bim-panel label="Settings" class="options-menu">
+          <bim-panel-section collapsed label="Lights">
           
             <bim-color-input 
               label="Background Color" color="#202932" 
@@ -96,6 +114,59 @@ function Index() {
             </bim-number-input>
             
           </bim-panel-section>
+        <bim-panel-section collapsed label="Clipper">
+
+          <bim-checkbox label="Clipper enabled" checked 
+            @change="${({ target }: { target: BUI.Checkbox }) => {
+              clipper.enabled = target.value;
+            }}">
+          </bim-checkbox>
+          
+          <bim-checkbox label="Clipper visible" checked 
+            @change="${({ target }: { target: BUI.Checkbox }) => {
+              clipper.visible = target.value;
+            }}">
+          </bim-checkbox>
+        
+          <bim-color-input 
+            label="Planes Color" color="#202932" 
+            @input="${({ target }: { target: BUI.ColorInput }) => {
+              clipper.material.color.set(target.color);
+            }}">
+          </bim-color-input>
+          
+          <bim-number-input 
+            slider step="0.01" label="Planes opacity" value="0.2" min="0.1" max="1"
+            @change="${({ target }: { target: BUI.NumberInput }) => {
+              clipper.material.opacity = target.value;
+            }}">
+          </bim-number-input>
+          
+          <bim-number-input 
+            slider step="0.1" label="Planes size" value="5" min="2" max="10"
+            @change="${({ target }: { target: BUI.NumberInput }) => {
+              clipper.size = target.value;
+            }}">
+          </bim-number-input>
+          
+          <bim-button 
+            label="Delete all" 
+            @click="${() => {
+              clipper.deleteAll();
+            }}">  
+          </bim-button>        
+          
+          <bim-button 
+            label="Rotate cube" 
+            @click="${() => {
+              cube.rotation.x = 2 * Math.PI * Math.random();
+              cube.rotation.y = 2 * Math.PI * Math.random();
+              cube.rotation.z = 2 * Math.PI * Math.random();
+            }}">  
+          </bim-button>
+         
+          
+        </bim-panel-section>
         </bim-panel>
         `;
     });
@@ -119,6 +190,7 @@ function Index() {
     document.body.append(button);
 
     return () => {
+      clipper.deleteAll();
       components.dispose();
     };
   }, []);
